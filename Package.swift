cd0b2faa7d6249ec3fdb73759a71f4eb1b2c5f8a
Prince Ugwuh <prince.ugwuh@gmail.com>
2025-11-22 20:50:49 -0600

    Removed comments in Package.swift

--- Package.swift
+++ Package.swift
@@ -7,15 +7,12 @@ let package = Package(
     name: "entrust",
     platforms: [.macOS(.v15)],
     products: [
-        // Expose the executable for Mint and other package managers
         .executable(name: "entrust", targets: ["entrust"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "entrust",
             dependencies: [
